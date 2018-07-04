0bc7baf9024e64116d8717ead3fd69aa47814787
MottledOne <byfinck@gmail.com>
2018-07-03 23:00:14 -0700

    just testing

--- titanic.R
+++ titanic.R
@@ -3,6 +3,3 @@ setwd("C:\\Users\\Benjamin\\kaggle\\titanic\\")
 train <- read.csv(".\\data\\train.csv")
 test <- read.csv(".\\data\\test.csv")
 gender_sub <- read.csv(".\\data\\gender_submission.csv")
-
-something
-somethingElse
